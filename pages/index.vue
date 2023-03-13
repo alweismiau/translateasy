3b422e296e832aa00ad2072402a811f25caa8c5c
Naraduhita <brigitanara97@gmail.com>
2023-03-13 19:58:23 +0700

    Update Submite

--- pages/index.vue
+++ pages/index.vue
@@ -69,11 +69,11 @@
             <option value="DE">Germany</option>
           </select>
           <!--Switch-->
-          <div class="rounded-full bg-slate px-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 mx-auto rounded-full bg-slate-300 p-2.5">
+          <button type="button" class="rounded-full bg-slate px-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 mx-auto rounded-full bg-slate-300 p-3 hover:bg-gray-400">
               <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
             </svg>
-          </div>
+          </button>
           <select
             id="countries"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 -mx-2"
@@ -104,14 +104,27 @@
       </div>
 
       <!--Submit-->
-      <div class="mt-4 mx-8 mb-8 p-2 font-inter grid grid-cols-3">
+      <div class="mt-4 mx-8 mb-8 p-2 font-inter grid grid-cols-5">
         <label></label>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 mx-40 rounded-full bg-slate-300 p-4 hover:bg-gray-200">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z"
+            />
+          </svg>
+        </button>
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white bg-pink-500 hover:bg-blue focus:ring-4 focus:outline-none focus:ring-pink-500 font-medium rounded-lg text-base w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-500 dark:hover:bg-pink-400 dark:focus:ring-pink-500"
         >
           Submit
         </button>
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14 mx-14 rounded-full bg-slate-300 p-4 hover:bg-gray-200">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+        </svg>
       </div>
       <!-- <div class="py-5 px-10">
       <input
