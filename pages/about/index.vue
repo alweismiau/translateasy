389758c21c592b1f15e60b0d34ec71dd6bc97638
alweismiau <keizafina@gmail.com>
2023-03-26 12:17:40 +0700

    download

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -148,8 +148,8 @@
           >
             <div class="flex justify-center">
               <img
-              src="~/assets/teams/agoy.jpg"
-                class="w-32 lg:w-20"
+              src="/images/about/bg-circle-fahrizal.png"
+                class="w-32 lg:w-40"
                 alt="Athallah Narda Wiyoga"
               />
             </div>
